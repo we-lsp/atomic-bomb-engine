0df237d4a2456347c2889a785b53056314f6339f
qyzhg <qyzhg@qyzhg.com>
2024-04-24 14:24:04 +0800

    feat: 许可中心增加模式匹配

--- src/core/concurrency_controller.rs
+++ src/core/concurrency_controller.rs
@@ -23,34 +23,37 @@ impl ConcurrencyController {
 
     // 分发许可证
     pub async fn distribute_permits(&self) {
-        if let Some(step_option) = &self.step_option {
-            let mut permits_added = 0usize;
-            {
-                let mut fractional_accumulator = self.fractional_accumulator.lock().await;
-                *fractional_accumulator += step_option.increase_step;
-                if *fractional_accumulator >= 1.0 {
-                    let initial_permits_to_add = fractional_accumulator.floor() as usize;
-                    self.semaphore.add_permits(initial_permits_to_add);
-                    permits_added += initial_permits_to_add;
-                    *fractional_accumulator -= initial_permits_to_add as f64;
+        match &self.step_option {
+            Some(step_option) => {
+                let mut permits_added = 0usize;
+                {
+                    let mut fractional_accumulator = self.fractional_accumulator.lock().await;
+                    *fractional_accumulator += step_option.increase_step;
+                    if *fractional_accumulator >= 1.0 {
+                        let initial_permits_to_add = fractional_accumulator.floor() as usize;
+                        self.semaphore.add_permits(initial_permits_to_add);
+                        permits_added += initial_permits_to_add;
+                        *fractional_accumulator -= initial_permits_to_add as f64;
+                    }
+                }
+                while permits_added < self.total_permits {
+                    tokio::time::sleep(Duration::from_secs(step_option.increase_interval)).await;
+                    let mut fractional_accumulator = self.fractional_accumulator.lock().await;
+                    *fractional_accumulator += step_option.increase_step;
+                    let permits_to_add = min(
+                        fractional_accumulator.floor() as usize,
+                        self.total_permits - permits_added,
+                    );
+                    if permits_to_add > 0 {
+                        self.semaphore.add_permits(permits_to_add);
+                        permits_added += permits_to_add;
+                        *fractional_accumulator -= permits_to_add as f64;
+                    }
                 }
             }
-            while permits_added < self.total_permits {
-                tokio::time::sleep(Duration::from_secs(step_option.increase_interval)).await;
-                let mut fractional_accumulator = self.fractional_accumulator.lock().await;
-                *fractional_accumulator += step_option.increase_step;
-                let permits_to_add = min(
-                    fractional_accumulator.floor() as usize,
-                    self.total_permits - permits_added,
-                );
-                if permits_to_add > 0 {
-                    self.semaphore.add_permits(permits_to_add);
-                    permits_added += permits_to_add;
-                    *fractional_accumulator -= permits_to_add as f64;
-                }
+            None => {
+                self.semaphore.add_permits(self.total_permits);
             }
-        } else {
-            self.semaphore.add_permits(self.total_permits);
         }
     }
 
