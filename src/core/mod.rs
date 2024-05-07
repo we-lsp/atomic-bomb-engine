48e04c3071a2bb92a088cc217212e98e10bba470
qyzhg <qyzhg@qyzhg.com>
2024-05-07 20:38:27 +0800

    feat: run_batch设置为公共

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -2,7 +2,7 @@ pub mod batch;
 pub mod check_endpoints_names;
 mod concurrency_controller;
 mod listening_assert;
-mod run_batch;
+pub mod run_batch;
 mod setup;
 mod share_result;
 pub mod sleep_guard;
